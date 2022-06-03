59b9728935fd5e6d6425cdef5edea3b0f9764a30
sachidanand <pythonlead@moweb.com>
2022-06-03 22:44:38 +0530

    set to sqlite db

--- Pmtools/settings.py
+++ Pmtools/settings.py
@@ -84,18 +84,25 @@ WSGI_APPLICATION = 'Pmtools.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'pmtools',
+#         'USER': 'postgres',
+#         'PASSWORD': 'neo',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+
+#     }
+
+# }
+
 DATABASES = {
-
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'pmtools',
-        'USER': 'postgres',
-        'PASSWORD': 'neo',
-        'HOST': 'localhost',
-        'PORT': '5432',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-
 }
 
 
